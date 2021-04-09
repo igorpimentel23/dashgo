dfa8ae47542c33bb843dd40a19924f8f98a1d909
Igor <igorpimentel@id.uff.br>
2021-04-09 17:39:58 -0300

    Navigation flux

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,24 +1,28 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import {
-  Link,
+  Link as ChakraLink,
   Icon,
   Text,
   LinkProps as ChakraLinkProps,
 } from '@chakra-ui/react';
+import Link from 'next/link';
 import { ElementType } from 'react';
 
 interface NavLinkProps extends ChakraLinkProps {
   icon: ElementType;
   title: string;
+  href: string;
 }
 
-export function NavLink({ icon, title, ...rest }: NavLinkProps) {
+export function NavLink({ icon, title, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">
-        {title}
-      </Text>
+    <Link href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">
+          {title}
+        </Text>
+      </ChakraLink>
     </Link>
   );
 }
